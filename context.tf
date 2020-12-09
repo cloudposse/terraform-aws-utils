b72bd934654d306ae22ab80443fa26c10131bf7a
Matt Calhoun <matt@mattcalhoun.com>
2020-12-09 14:10:00 -0500

    update context to latest

--- context.tf
+++ context.tf
@@ -1,9 +1,3 @@
-#####################################################################
-## Because of the static nature of this module, this file          ##
-## has been modified to indicate that the context.tf variables     ##
-## are accepted but ignored. When updating this file, please       ##
-## update the variable descriptions accordingly.                   ##
-#####################################################################
 #
 # ONLY EDIT THIS FILE IN github.com/cloudposse/terraform-null-label
 # All other instances of this file should be a copy of that one
@@ -24,8 +18,10 @@
 # will be null, and `module.this.delimiter` will be `-` (hyphen).
 #
 
+
 module "this" {
-  source = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.19.2"
+  source  = "cloudposse/label/null"
+  version = "0.22.0" // requires Terraform >= 0.12.26
 
   enabled             = var.enabled
   namespace           = var.namespace
@@ -75,45 +71,49 @@ variable "context" {
     id_length_limit     = null
   }
   description = <<-EOT
-    NOTE: This object and associated variables are provided for compatibility with other Cloud Posse modules but **ARE NOT USED** at present.
-    EOT
+    Single object for setting entire context at once.
+    See description of individual variables for details.
+    Leave string and numeric variables as `null` to use default value.
+    Individual variable settings (non-null) override settings in context object,
+    except for attributes, tags, and additional_tag_map, which are merged.
+  EOT
 }
 
 variable "enabled" {
   type        = bool
   default     = null
-  description = "(Ignored) Set to false to prevent the module from creating any resources"
+  description = "Set to false to prevent the module from creating any resources"
 }
 
 variable "namespace" {
   type        = string
   default     = null
-  description = "(Ignored) Namespace, which could be your organization name or abbreviation, e.g. 'eg' or 'cp'"
+  description = "Namespace, which could be your organization name or abbreviation, e.g. 'eg' or 'cp'"
 }
 
 variable "environment" {
   type        = string
   default     = null
-  description = "(Ignored) Environment, e.g. 'uw2', 'us-west-2', OR 'prod', 'staging', 'dev', 'UAT'"
+  description = "Environment, e.g. 'uw2', 'us-west-2', OR 'prod', 'staging', 'dev', 'UAT'"
 }
 
 variable "stage" {
   type        = string
   default     = null
-  description = "(Ignored) Stage, e.g. 'prod', 'staging', 'dev', OR 'source', 'build', 'test', 'deploy', 'release'"
+  description = "Stage, e.g. 'prod', 'staging', 'dev', OR 'source', 'build', 'test', 'deploy', 'release'"
 }
 
 variable "name" {
   type        = string
   default     = null
-  description = "(Ignored) Solution name, e.g. 'app' or 'jenkins'"
+  description = "Solution name, e.g. 'app' or 'jenkins'"
 }
 
 variable "delimiter" {
   type        = string
   default     = null
   description = <<-EOT
-    (Ignored) Delimiter to be used between `namespace`, `environment`, `stage`, `name` and `attributes`.
+    Delimiter to be used between `namespace`, `environment`, `stage`, `name` and `attributes`.
     Defaults to `-` (hyphen). Set to `""` to use no delimiter at all.
   EOT
 }
@@ -121,26 +121,26 @@ variable "delimiter" {
 variable "attributes" {
   type        = list(string)
   default     = []
-  description = "(Ignored) Additional attributes (e.g. `1`)"
+  description = "Additional attributes (e.g. `1`)"
 }
 
 variable "tags" {
   type        = map(string)
   default     = {}
-  description = "(Ignored) Additional tags (e.g. `map('BusinessUnit','XYZ')`"
+  description = "Additional tags (e.g. `map('BusinessUnit','XYZ')`"
 }
 
 variable "additional_tag_map" {
   type        = map(string)
   default     = {}
-  description = "(Ignored) Additional tags for appending to tags_as_list_of_maps. Not added to `tags`."
+  description = "Additional tags for appending to tags_as_list_of_maps. Not added to `tags`."
 }
 
 variable "label_order" {
   type        = list(string)
   default     = null
   description = <<-EOT
-    (Ignored) The naming order of the id output and Name tag.
+    The naming order of the id output and Name tag.
     Defaults to ["namespace", "environment", "stage", "name", "attributes"].
     You can omit any of the 5 elements, but at least one must be present.
   EOT
@@ -150,7 +150,7 @@ variable "regex_replace_chars" {
   type        = string
   default     = null
   description = <<-EOT
-    (Ignored) Regex to replace chars with empty string in `namespace`, `environment`, `stage` and `name`.
+    Regex to replace chars with empty string in `namespace`, `environment`, `stage` and `name`.
     If not set, `"/[^a-zA-Z0-9-]/"` is used to remove all characters other than hyphens, letters and digits.
   EOT
 }
@@ -159,7 +159,7 @@ variable "id_length_limit" {
   type        = number
   default     = null
   description = <<-EOT
-    (Ignored) Limit `id` to this many characters.
+    Limit `id` to this many characters.
     Set to `0` for unlimited length.
     Set to `null` for default, which is `0`.
     Does not affect `id_full`.
